c69526c5398c22086c6c7d10c0974b45d2ad625b
Hlo World <abhayabikramshahiofficial@gmail.com>
2024-12-08 16:45:12 +0545

    Your commit message here

--- js/button.js
+++ js/button.js
@@ -1,3 +1,8 @@
 document.getElementById("moree").addEventListener('click', function() {
-  window.location.href = 'https://badimalikasecschool.netlify.app';
+  window.location.href = 'https://badimalikasecschool.netlify.app/pages/about-us/about';
 });
+
+document.getElementById("imp").addEventListener('click', function() {
+  window.location.href = 'https://badimalikasecschool.netlify.app/s/apply/bmms';
+});
+
